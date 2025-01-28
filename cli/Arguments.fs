5f85b45c33111c8ecccd8d882ee5f37dd189284f
Morgan Kenyon <morganwkenyon@gmail.com>
2025-01-27 23:08:36 -0600

    Getting started on cli tool for waux

---
+++ cli/Arguments.fs
@@ -0,0 +1,45 @@
+﻿namespace Waux.Lang.Cli
+
+module Arguments =
+    open Argu
+
+    type CliError =
+    | ArgumentsNotSpecified
+
+    [<CliPrefix(CliPrefix.Dash)>]
+    type CompileArgs =
+        | Filename of string
+        interface IArgParserTemplate with
+            member this.Usage =
+                match this with
+                | Filename _ -> "The .waux file needing to compile"
+
+    type CmdArgs =
+        | [<AltCommandLine("-p")>] Print of message:string
+        | [<CliPrefix(CliPrefix.None)>] Compile of ParseResults<CompileArgs>
+    with
+        interface IArgParserTemplate with
+            member this.Usage =
+                match this with
+                | Print _ -> "Print a message"
+                | Compile _ -> "Compile a .waux file to .wasm"
+
+    let getExitCode result =
+        match result with
+        | Ok () -> 0
+        | Error err ->
+            match err with
+            | ArgumentsNotSpecified -> 1
+
+    let runPrint print = 
+        printfn "%s" print
+        Ok ()
+
+    let compile (parseResults: ParseResults<CompileArgs>) =
+        match parseResults with
+        | f when f.Contains(Filename) ->
+            let filename = f.GetResult(Filename)
+            printfn "Compiling: %s" filename
+            Ok ()
+        | _ -> Error ArgumentsNotSpecified
+
---
+++ cli/Program.fs
@@ -0,0 +1,21 @@
+﻿namespace Waux.Lang.Cli
+
+module Program = 
+    open Argu
+    open Arguments
+    open System
+
+    [<EntryPoint>]
+    let main argv = 
+        let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
+        let parser = ArgumentParser.Create<CmdArgs>(programName = "waux", errorHandler = errorHandler)
+    
+        match parser.ParseCommandLine argv with
+        | p when p.Contains(Print) -> runPrint (p.GetResult(Print))
+        | c when c.Contains(Compile) -> 
+            let cc = c.GetResult(Compile)
+            compile cc
+        | _ ->
+            printfn "%s" (parser.PrintUsage())
+            Error ArgumentsNotSpecified
+        |> getExitCode
